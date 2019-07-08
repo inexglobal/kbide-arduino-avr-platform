9b6385ce916ef3f2c20051a14a825107dc916a19
comdet <listzone@hotmail.com>
2019-07-08 13:57:09 +0700

    update compiler to support all board. remove static libraries.

--- compiler.js
+++ compiler.js
@@ -36,8 +36,11 @@ const setConfig = (context) => {
   }
 
   G.cflags = G.cflags.map(f => f.replace(/\{platform\}/g, platformDir));
+  G.cppflags = G.cppflags.map(f => f.replace(/\{platform\}/g, platformDir));
   G.ldflags = G.ldflags.map(f => f.replace(/\{platform\}/g, platformDir));
   G.ldlibflag = G.ldlibflag.map(f => f.replace(/\{platform\}/g, platformDir));
+  G.core_files = fs.readdirSync(`${platformDir}/sdk/cores/arduino`).filter(f=> f.endsWith(".cpp") || f.endsWith(".c"));
+  G.core_files = G.core_files.map(f => `${platformDir}/sdk/cores/arduino/${f}`);
 
   G.COMPILER_AR = `${platformDir}/${G.toolchain_dir}/avr-ar`;
   G.COMPILER_GCC = `${platformDir}/${G.toolchain_dir}/avr-gcc`;
@@ -52,25 +55,30 @@ const setConfig = (context) => {
   G.ARCHIVE_FILE = `${G.app_dir}/libmain.a`;
 };
 
-const compileFiles = function(sources, boardCppOptions, boardcflags, plugins_includes_switch) {
+const compileFiles = async function(sources, boardCppOptions, boardcflags, plugins_includes_switch) {
   console.log(`arduino-avr compiler.compileFiles`);
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
     let cflags = `${G.cflags.join(" ")} ${boardcflags.join(" ")}`;
     let cppOptions = G.cpp_options.join(" ") + boardCppOptions.join(" ");
     let inc_switch = plugins_includes_switch.map(obj => `-I"${obj}"`).join(" ");
-    let debug_opt = G.arch ? ("-mmcu=" + G.arch) : "";
-    debug_opt += G.cpu_clock ? ("-DF_CPU=" + G.cpu_clock) : "";
-    debug_opt += G.arduino_version ? ("-DARDUINO=" + G.arduino_version) : "";
+    let debug_opt = G.board_context.arch ? (" -mmcu=" + G.board_context.mcu) : "";
+    debug_opt += G.board_context.cpu_clock ? (" -DF_CPU=" + G.board_context.cpu_clock) : "";
+    debug_opt += G.board_context.arduino_version ? (" -DARDUINO=" + G.board_context.arduino_version) : "";
+    debug_opt += " -DARDUINO_"+ G.board_context.arch +" -DARDUINO_ARCH_";
     let finalFiles = [];
     console.log(`arduino-avr/compiler.js`);
 	
-	fs.copyFileSync(`${platformDir}/main.cpp`, `${G.app_dir}/main.cpp`);
+	  fs.copyFileSync(`${platformDir}/main.cpp`, `${G.app_dir}/main.cpp`);
   	sources.push(`${G.app_dir}/main.cpp`);
-  
-    sources.forEach(async (file, idx, arr) => {
+    sources = sources.concat(G.core_files);
+  	//console.log(G.core_files);
+    for(let i in sources){
+      let file = sources[i];
       let filename = getName(file);
       let fn_obj = `${G.app_dir}/${filename}.o`;
-      let cmd = `"${G.COMPILER_CPP}" ${cppOptions} ${cflags} ${inc_switch} ${debug_opt} -c "${file}" -o "${fn_obj}"`;
+      let cmd_c = `"${G.COMPILER_GCC}" ${cppOptions} ${cflags} ${inc_switch} ${debug_opt} -c "${file}" -o "${fn_obj}"`;
+      let cmd_cpp = `"${G.COMPILER_CPP}" ${cppOptions} ${cflags} ${inc_switch} ${debug_opt} -c "${file}" -o "${fn_obj}"`;
+      let cmd = file.endsWith(".c")? cmd_c : cmd_cpp;
       try {
         console.log("comping => " + file);
         const {stdout, stderr} = await execPromise(ospath(cmd), {cwd: G.process_dir});
@@ -97,7 +105,8 @@ const compileFiles = function(sources, boardCppOptions, boardcflags, plugins_inc
         };
         reject(_e);
       }
-    });
+    }
+
   });
 };
 
@@ -105,10 +114,16 @@ function linkObject(ldflags, extarnal_libflags) {
   console.log(`linking... ${G.ELF_FILE}`);
   G.cb(`linking... ${G.ELF_FILE}`);
   let flags = G.ldflags.concat(ldflags);
-  let libflags = (extarnal_libflags) ? G.ldlibflag.concat(extarnal_libflags).
-  join(" ") : G.ldlibflag.join(" ");
+  let libflags = (extarnal_libflags) ? G.ldlibflag.concat(extarnal_libflags).join(" ") : G.ldlibflag.join(" ");
   flags = G.ldflags.join(" ") + " " + ldflags.join(" ");
-  let cmd = `"${G.COMPILER_GCC}" ${flags} -Wl,--start-group ${libflags} -L"${G.app_dir}" -Wl,--end-group -Wl,-EL -o "${G.ELF_FILE}"`;
+  let obj_files = fs.readdirSync(G.app_dir).filter(f=>f.endsWith(".o"));
+  obj_files = obj_files.map(f=> `${G.app_dir}/${f}`);
+  obj_files = obj_files.join(" ");
+  let debug_opt = G.board_context.arch ? (" -mmcu=" + G.board_context.mcu) : "";
+  debug_opt += G.board_context.cpu_clock ? (" -DF_CPU=" + G.board_context.cpu_clock) : "";
+  debug_opt += G.board_context.arduino_version ? (" -DARDUINO=" + G.board_context.arduino_version) : "";
+  debug_opt += " -DARDUINO_"+ G.board_context.arch +" -DARDUINO_ARCH_";
+  let cmd = `"${G.COMPILER_GCC}" ${flags} ${debug_opt} -o ${G.ELF_FILE} ${obj_files} -L${G.app_dir} -lm"`;
   return execPromise(ospath(cmd), {cwd: G.process_dir});
 }
 
