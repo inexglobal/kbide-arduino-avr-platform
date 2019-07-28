770941891516ed60529384a190f60aa28b619703
comdet <listzone@hotmail.com>
2019-07-28 11:14:31 +0700

    stage change new version to 1.0.1

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
     auther : 'Maker Asia',
     website : 'https://github.com/MakerAsia/KBProIDE',
     git : 'https://github.com/MakerAsia/kbide-arduino-avr-platform',
-    version : '1.0.0',
+    version : '1.0.1',
     data : {
         name : 'arduino-avr',
         platform : 'arduino-avr',
